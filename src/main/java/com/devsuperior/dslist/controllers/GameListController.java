1e21354744f169576b615785b4b0ed042a22bd83
Andherson Costa <00andherson@gmail.com>
2025-05-22 12:07:20 -0300

    GET lists endpoint

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.DTO.GameDTO;
+import com.devsuperior.dslist.DTO.GameListDTO;
+import com.devsuperior.dslist.DTO.GameMinDTO;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll(){
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+
+import com.devsuperior.dslist.DTO.GameListDTO;
+import com.devsuperior.dslist.DTO.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+        return dto;
+    }
+
+}
